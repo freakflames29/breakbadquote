5a941375e503e9f88c1ce5d1e795ed4f78407a8e
Sourav Das <iamsouravdaswade420@gmail.com>
2024-07-07 16:38:09 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by sourav das on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
